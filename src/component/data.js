82101c5b7e75b0d20043a1944449948ed3b81d20
Tensae Kebede <tensaekebede@Tensaes-MacBook-Pro-2.local>
2023-10-01 01:26:49 -0700

    moved portfolio proj up

--- src/component/data.js
+++ src/component/data.js
@@ -6,6 +6,12 @@ export const projectData = [
     I created Kubernetes YAML files to define deployments, services, and pods. To host the application, I established a Google Kubernetes Engine (GKE) cluster through Google Cloud Console.`,
   },
   {
+    id: 3,
+    title: "Portfolio",
+    description:
+      "Developed a professional developer portfolio utilizing Next.js, a React-based framework, to showcase my technical skills and projects. Implemented seamless deployment by configuring continuous integration and deployment (CI/CD) pipelines, enabling automatic updates of my portfolio website directly from my GitHub repository to the Azure cloud platform.",
+  },
+  {
     id: 2,
     title: "Location Based Message Board",
     description: `I developed a geofencing application in Java that allows users to post and view comments based on their proximity to specific landmarks. This application utilizes the Google Location API to calculate users' distances from various landmarks in real-time. To ensure the reliability of the geolocation features, 
@@ -16,11 +22,5 @@ export const projectData = [
     title: "Engine for generating explorable worlds",
     description:
       "I developed a Java-based engine that generates intricate 2D pseudo-random worlds with interconnected rooms and hallways. This involved implementing advanced algorithms, such as depth-first search, to dynamically create complex layouts for these worlds. To ensure the accuracy of the world generation algorithms, I conducted thorough unit testing using the JUnit testing framework.",
-  },
-  {
-    id: 3,
-    title: "Portfolio",
-    description:
-      "Developed a professional developer portfolio utilizing Next.js, a React-based framework, to showcase my technical skills and projects. Implemented seamless deployment by configuring continuous integration and deployment (CI/CD) pipelines, enabling automatic updates of my portfolio website directly from my GitHub repository to the Azure cloud platform.",
   },
 ];
